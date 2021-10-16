2e613e9c65b1634225fa5c5b81be2aeb772f8c29
Shuting Mei <69299696+ShutingMei@users.noreply.github.com>
2021-10-16 16:52:00 +0800

    Add files via upload

---
+++ Batch_2ndLevel.m
@@ -0,0 +1,37 @@
+function matlabbatch = Batch_2ndLevel(Datapath, Savepath, Conname, Design)
+%% 2nd Level                                          Shuting Mei
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Before this step, you must run 1st level analysis, and generate con files
+% at least FOUR subjects to conduct the 2nd level analysis.
+%
+% Please note that the order of the con file would be consistent with your 
+% 1st level analysis. e.g. in my Experiment, con_0001 means the Self-Friend
+% condition.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% fMRI model specification
+matlabbatch{1}.spm.stats.factorial_design.dir = {Savepath};
+volumes = spm_select('List',Datapath,Conname);  %ExtList for 4D; List for 3D
+volumesDir = fullfile(Datapath,cellstr(volumes));
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = volumesDir;
+
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+
+%% Model estimation
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+
+%% Contrast management
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = Design.name;
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = Design.weights;
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'repl';
+matlabbatch{3}.spm.stats.con.delete = 0;
+end
